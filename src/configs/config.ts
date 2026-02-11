9c01194ec6dedfa273e14e8a83f7376ca5c0fd2c
Gagun45 <selyanchyn45@gmail.com>
2026-02-11 12:33:35 +0200

    db connection

---
+++ src/configs/config.ts
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+interface IConfig {
+    PORT: string;
+    MONGO_URI: string;
+}
+
+export const config: IConfig = {
+    PORT: process.env.PORT,
+    MONGO_URI: process.env.MONGO_URI,
+}
